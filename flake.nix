07f4f78b919346779091aa5d568e8daef1604ca8
Jakku Night <R101g117l117s@gmail.com>
2025-02-04 09:01:10 -0300

    Add missing import.

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
   in
   {
     packages.${system} = {
-      grub-theme = ./wanderer-genshin-grub-theme { inherit pkgs; };
+      grub-theme = import ./wanderer-genshin-grub-theme { inherit pkgs; };
     };
   };
 }
